a589edfe911a2d455c91affca63901ef3453e95b
Caner Tüysüz <canertuysuz@hotmail.com>
2023-10-22 16:03:14 +0300

    Add Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Button } from "./ui/button"
+import { TimerIcon } from "@radix-ui/react-icons"
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between p-5">
+      <div className="flex items-center">
+        <TimerIcon width={30} height={30} />
+        <div className="text-2xl justify-center">
+          <span className="font-medium">Time</span>
+          <span className="font-bold">Log</span>
+        </div>
+      </div>
+      <Button>Logout</Button>
+    </div>
+  )
+}
