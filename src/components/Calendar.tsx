23763c7145a3a9a13996bedeb36bbd4c0e8baf8e
Caner Tüysüz <canertuysuz@hotmail.com>
2023-10-23 17:18:01 +0300

    Fix date box hover issue

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -22,7 +22,6 @@ function Calendar() {
     return dates
   }
 
-  // TODO Fix the card appears when date boxes hovered
   return (
     <div className="flex flex-wrap justify-center border-[1px] border-solid gap-2 p-10 rounded-md">
       {getDateInMonth().map((date, index) => {
@@ -31,7 +30,15 @@ function Calendar() {
             <HoverCardTrigger>
               <div className="h-5 w-5 bg-gray-100 rounded-sm cursor-pointer"></div>
             </HoverCardTrigger>
-            <HoverCardContent>0 hours on {date}</HoverCardContent>
+            <HoverCardContent
+              className="z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground 
+              shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out 
+              data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 
+              data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 
+              data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+            >
+              0 hours on {date}
+            </HoverCardContent>
           </HoverCard>
         )
       })}
